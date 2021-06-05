c92b0c93456c79bb36e8a0cb5a5ca204517e2b2b
gillesshi <gillesshi@leyinetwork.com>
2021-06-05 18:38:10 +0800

    feat: change post station position

--- gillescommon/position/images.py
+++ gillescommon/position/images.py
@@ -64,3 +64,6 @@ dispatch_2_me = Template(r"dispatch_me.png", record_pos=(0.309, -0.101), resolut
 dispatch_2_me = Template(r"dispatch_me.png", record_pos=(0.309, -0.101), resolution=(1440, 811))
 dispatch_3_me = Template(r"dispatch_me.png", record_pos=(0.324, -0.019), resolution=(1440, 811))
 cross_2_me = Template(r"cross_2_me.png", record_pos=(0.356, -0.227), resolution=(1440, 811))
+
+
+
